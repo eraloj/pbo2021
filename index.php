f4a76fc6773bb100c7aaf9d3f4a4d16f295f8509
Eraldo <eraldoaldo43@gmail.com>
2021-02-18 21:08:23 +0700

    praktikum 02 membuat objek dan class

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+include("Mahasiswa.php");
+$eraldo = new Mahasiswa();
+$padel = new Mahasiswa();
+$ipan = new Mahasiswa();
+
+$eraldo->nim = "H1101191026";
+$eraldo->nama = "Eraldo Jonathan Ramadhan";
+$eraldo->tanggal_lahir = "18 Desember 2000";
+$eraldo->jenis_kelamin = "Laki - Laki";
+
+$padel->nim = "H1101191034";
+$padel->nama = "Muhammad Fadilah";
+$padel->tanggal_lahir = "03 Januari 2001";
+$padel->jenis_kelamin = "Laki - Laki";
+
+$ipan->nim = "H1101191062";
+$ipan->nama = "Muhammad Pramudia Irvan Chaniago";
+$ipan->tanggal_lahir = "05 Mei 2001";
+$ipan->jenis_kelamin = "Laki - Laki";
+
